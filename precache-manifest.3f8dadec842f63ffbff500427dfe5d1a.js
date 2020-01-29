15e01fdae898fe498921354b2b07bbe1e92bbde5
Meet Panchal <connect.meetpanchal@gmail.com>
2020-01-29 21:27:43 +0530

    Updates

---
+++ precache-manifest.3f8dadec842f63ffbff500427dfe5d1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef61560d9145e714000275aa47f99902",
+    "url": "/react-cartExample/index.html"
+  },
+  {
+    "revision": "a73ceda28e30a8a0d021",
+    "url": "/react-cartExample/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "8b136e4142315e566860",
+    "url": "/react-cartExample/static/css/main.6c34e071.chunk.css"
+  },
+  {
+    "revision": "a73ceda28e30a8a0d021",
+    "url": "/react-cartExample/static/js/2.093f3331.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-cartExample/static/js/2.093f3331.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8b136e4142315e566860",
+    "url": "/react-cartExample/static/js/main.cabcb24c.chunk.js"
+  },
+  {
+    "revision": "2b8a89bd07c8acada37c",
+    "url": "/react-cartExample/static/js/runtime-main.f814b812.js"
+  }
+]);
